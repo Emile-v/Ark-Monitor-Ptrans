44578bce5d66b364a64e3d8963ab4567bc26edff
e19b639q <emile.vigin@etu.univ-nantes.fr>
2021-02-09 19:12:51 +0100

    numberOfNodes ByBlockId, ByHeight, ByVersion

--- allConnectedPeer.js
+++ allConnectedPeer.js
@@ -10,10 +10,7 @@ async function fetchAsync (url) {
 }
 
 
-/**  cette fonction ajoute dans la listOfAllPeer les peers dont le @arkcoresystem = 4003
- *  Le paramètre numPage définit le numéro de page sur laquelle on récupère les peers sur l'api
- * */
-async function callMainNodeApiPeers(All4003NodeIPs, AllNodeIPs, numPage){
+async function callMainNodeApiPeers(All4003NodeIPs, AllNodeIPs, AllNodesInfos, numPage){
     let stop = false;
     let res = fetchAsync(`https://api.ark.io/api/peers?page=${numPage}&limit=100`);
     let res2 = await res.then(res => {
@@ -23,6 +20,7 @@ async function callMainNodeApiPeers(All4003NodeIPs, AllNodeIPs, numPage){
         }
         else{
             res.data.forEach(element=>{
+                AllNodesInfos.push(element);
                 AllNodeIPs.push(element.ip);
                 if (element.ports['@arkecosystem/core-api'] == 4003){
                     All4003NodeIPs.push(element.ip);   
@@ -34,13 +32,13 @@ async function callMainNodeApiPeers(All4003NodeIPs, AllNodeIPs, numPage){
     return stop
 }
 
-async function getIPsFromApiMainNode(All4003NodeIPs, AllNodeIPs){
+async function getIPsFromApiMainNode(All4003NodeIPs, AllNodeIPs, AllNodesInfos){
     
     let numPage = 1;
     let stop = false;
 
     while(stop == false){
-        stop = await callMainNodeApiPeers(All4003NodeIPs, AllNodeIPs, numPage);
+        stop = await callMainNodeApiPeers(All4003NodeIPs, AllNodeIPs, AllNodesInfos, numPage);
         numPage++;
     }
 
@@ -75,15 +73,17 @@ async function getPeers(nodeIP){
     return listOfAllPeers;
 }
 
-async function fillUpWithPeers(All4003NodeIPs, AllNodeIPs){
+async function fillUpWithPeers(All4003NodeIPs, AllNodeIPs, AllNodesInfos){
     
     for (let i=0; i<All4003NodeIPs.length; i++) {   
-        let resPeers = await getPeers(All4003NodeIPs[i]);        
-        resPeers.forEach(element =>{
+        let resPeers = await getPeers(All4003NodeIPs[i]);      
+        resPeers.forEach(element =>{ 
             let peerIp = element.ip;
             if (!AllNodeIPs.includes(peerIp)){
-                console.log("here");
+                //console.log("here");
+                console.log(All4003NodeIPs[i]);
                 AllNodeIPs.push(peerIp);
+                AllNodesInfos.push(element);
                 if (element.ports['@arkecosystem/core-api'] == 4003){
                     All4003NodeIPs.push(peerIp)
                 }
@@ -112,20 +112,23 @@ async function getAllNodeIPs(){
 
     let All4003NodeIPs = [];
     let AllNodeIPs = [];
+    let AllNodesInfos = [];
 
-    await getIPsFromApiMainNode(All4003NodeIPs, AllNodeIPs);
+    await getIPsFromApiMainNode(All4003NodeIPs, AllNodeIPs, AllNodesInfos);
 
-    await fillUpWithPeers(All4003NodeIPs, AllNodeIPs);  
+    //All4003NodeIPs.push("85.235.64.251");
+    await fillUpWithPeers(All4003NodeIPs, AllNodeIPs, AllNodesInfos);  
     
     let NodeIPs = [All4003NodeIPs, AllNodeIPs];
 
     console.log(NodeIPs[0].length);
     console.log(NodeIPs[1].length);
+    //console.log(AllNodesInfos);
 
-    return NodeIPs;
+    return AllNodesInfos;
 }
 
-getAllNodeIPs();
+//getAllNodeIPs();
 
 /** cette fonction affiche (ou peut retrouner) la liste des peers avec leur état (synced = true/false) 
  * l'excecution de cette fonction peut prendre une vaingtaine de sec
@@ -159,3 +162,91 @@ async function verifStatusV2(){
 let L2 = [{1:2},{7:3}];
 L1.push(...L2);
 console.log(L1); */
+
+
+
+
+
+async function allVersion (nodeList){
+
+    let result = {};
+    //let res = fetchAsync(`https://api.ark.io/api/peers?page=1&limit=100`);
+
+    let temp = await nodeList.forEach(elem => {   
+        let version = elem.version;
+        if(!result[`${version}`]){
+            result[`${version}`] = 1;
+        }
+        else{
+            result[`${version}`]++;
+        }
+    });
+
+    return result;
+}
+
+async function testVersion(){
+    let nodeList = await getAllNodeIPs();
+    let result = await allVersion(nodeList);
+    console.log(result);
+}
+
+//testVersion();
+
+
+
+
+
+
+
+async function numberOfNodesByHeight(nodeList){
+
+    let result = {};
+    //let res = fetchAsync(`https://api.ark.io/api/peers?page=1&limit=100`);
+
+    let temp = await nodeList.forEach(elem => {   
+        let height = elem.height;
+        if(!result[`${height}`]){
+            result[`${height}`] = 1;
+        }
+        else{
+            result[`${height}`]++;
+        }
+    });
+
+    return result;
+}
+
+async function testHeight(){
+    let nodeList = await getAllNodeIPs();
+    let result = await numberOfNodesByHeight(nodeList);
+    console.log(result);
+}
+
+//testHeight();
+
+async function numberOfNodesByBlockId(nodeList){
+    let byHeight = await numberOfNodesByHeight(nodeList);
+    let result = {};
+    for (const [key, value] of Object.entries(byHeight)){
+        let res = await fetchAsync(`https://api.ark.io/api/blocks/${key}`);
+        let blockId = res.data.id;
+        result[`${blockId}`] = value;
+    }
+    return result;
+}
+
+
+async function testBlockId(){
+    let nodeList = await getAllNodeIPs();
+    let result = await numberOfNodesByBlockId(nodeList);
+    console.log(result);
+    console.log(Object.keys(result));
+}
+
+testBlockId();
+
+
+
+
+
