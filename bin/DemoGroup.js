706d385378e87cb1d84af9e2ac84856eab66242f
f-riviere <florent.riviere@etu.univ-nantes.fr>
2021-04-01 14:40:51 +0400

    group test

---
+++ bin/DemoGroup.js
@@ -0,0 +1,26 @@
+const listOfNodes = require('../listOfNodes.json');
+
+
+const { numberOfNodesByBlockId } = require('../Indicators/Global/numberOfNodesByBlockId');
+const { numberOfNodesByHeight } = require('../Indicators/Global/numberOfNodesByHeight');
+const { numberOfNodesByVersion } = require('../Indicators/Global/numberOfNodesByVersion');
+const { RetrieveTransaction } = require('../Indicators/Global/numberOfTransactions');
+
+
+async function DemoGroup(nodeList){
+    let obj=[];
+    obj.numberOfNodesByBlockId= await numberOfNodesByBlockId(nodeList);
+    obj.numberOfNodesByHeight= await numberOfNodesByHeight(nodeList);
+    obj.numberOfNodesByVersion= await numberOfNodesByVersion(nodeList);
+
+    return obj
+}
+
+
+async function test(){
+    res= await DemoGroup(listOfNodes);
+    // res.forEach(el => console.log(el))
+    console.log(res)
+}
+
+test();
