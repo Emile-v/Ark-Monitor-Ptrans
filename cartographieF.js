34c2fbeb9586d0ea662809da56f7d2c973b9adfe
LightTM <tran.mickaellumiere@gmail.com>
2021-04-26 02:52:29 +0200

    correction

--- cartographieF.js
+++ cartographieF.js
@@ -1,7 +1,7 @@
 const { list_All_Peers_Specific_Node, list_All_Peers_Specific_Node_Max_Peer, open_Port } 
 = require('./Indicators/Ark/Peers');
 
-const {exportDataYAML} = require('./utils/export')
+const {exportDataJSON, exportDataYAML} = require('./utils/export')
 
 const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -130,36 +130,14 @@ class Graph {
   async graphInit(iteration){
     let id_Node = 0 // increment the id of node
     let id_Edge = 0 // increment the id of node
+    let ctpIteration = 1 // optionnal
+    let tab_Initialised_IP = [this.racine.label]
+    let tab_Of_couple_Initialised = []
 
 
     if(await open_Port(this.racine.label) == true){    
 
-        /**first step */
-        if(iteration <= 1){
-            let tabIP = await list_All_Peers_Specific_Node(this.racine.label) // retourne tous les peers à partir d'un noeud
-
-            if(tabIP.length>0){
-                tabIP.forEach(async(elem) => {
-
-                    /**node init */
-                    id_Node++
-                    let node = new Node(id_Node, elem.ip)
-                    if(this.includes_Node(node)==false){
-                        this.add_Node(node)
-                    }
-
-                    /**edge init */
-                    let edge = new Edge(id_Edge, node, this.racine)
-                    if(this.includes_Edge(edge)==false){
-                        this.add_Edge(edge)
-                    }
-                    id_Edge++
-
-                    
-                })
-             }
-        }
-        else{
+        /**first step */       
             /** as the first step */
             let tabIP = await list_All_Peers_Specific_Node(this.racine.label) // retourne tous les peers à partir d'un noeud
             if(tabIP.length>0){
@@ -167,26 +145,39 @@ class Graph {
 
                     /**node init */
                     id_Node++
-                    let node = new Node(id_Node, elem.ip)
-                    if(this.includes_Node(node)==false){
+                    let node;
+                    if(tab_Initialised_IP.includes(elem.ip)==false){
+                        tab_Initialised_IP.push(elem.ip)
+                        node = new Node(id_Node, elem.ip)
                         this.add_Node(node)
+
+                        /**edge init */         
+                        let edge;
+                        /*  if(this.includes_Edge(edge)==false){ // %%%%%%%%%%%%%%%%%%%%%%%%%% un soucis ici*/
+                        let combined_key = this.racine.id + node.id 
+                        /** vu que les id sont uniques, 
+                         * en les additionnant on obtient egalement un nouvel id unique qui représente l'id des deux noeuds */
+
+                        if(tab_Of_couple_Initialised.includes(combined_key) == false){
+                            tab_Of_couple_Initialised.push(combined_key)
+                            edge = new Edge(id_Edge, this.racine, node)
+                            this.add_Edge(edge)
+                        }
+                        id_Edge++
                     }
 
-                    /**edge init */
                     
-                    let edge = new Edge(id_Edge, this.racine, node)
-                    if(this.includes_Edge(edge)==false){
-                        this.add_Edge(edge)
-                    }
-                    id_Edge++
 
+                    /** à décommenter pour voir plus d'information sur le graphe */
                     // console.log("nodes :")
                     // console.log(this.nodes.length)
                     // console.log("edges :")
-                    // console.log(this.edges.length)
-                    
-                })
-            }
+                    // console.log(this.edges.length)     
+            })
+             /**optionnelle */
+             console.log("fin de la " + ctpIteration + " iteration")
+             ctpIteration++
+
             while(iteration>1){
                 this.edges.forEach(async (edge) => {
                     if(await open_Port(edge.target.label) == true) {
@@ -196,20 +187,38 @@ class Graph {
 
                                 /**node init */
                                 id_Node++
-                                let node2 = new Node(id_Node, elem.ip)
-                                if(this.includes_Node(node2)==false){
+                                let node2;
+                                if(tab_Initialised_IP.includes(elem.ip)==false){
+                                    tab_Initialised_IP.push(elem.ip)
+                                    node2 = new Node(id_Node, elem.ip)
                                     this.add_Node(node2)
+
+
+                                    let edge2;
+                                    /*  if(this.includes_Edge(edge)==false){ // %%%%%%%%%%%%%%%%%%%%%%%%%% un soucis ici*/
+                                    let combined_key2 = edge.target.id + node2.id 
+                                    /** vu que les id sont uniques, 
+                                     * en les additionnant on obtient egalement un nouvel id unique qui représente l'id des deux noeuds */
+
+                                    if(tab_Of_couple_Initialised.includes(combined_key2) == false){
+                                        tab_Of_couple_Initialised.push(combined_key2)
+                                        edge2 = new Edge(id_Edge, edge.target, node2)
+                                        this.add_Edge(edge2)
+                                    }
+                                    id_Edge++
                                 }
             
                                 /**edge init */
-                                id_Edge++
-                                let edge2 = new Edge(id_Edge, edge.target, node2)
-                                if(this.includes_Edge(edge2)==false){
-                                    // console.log("yo")
-                                    this.add_Edge(edge2)
-                                }
+                                
+                                // let edge2 = new Edge(id_Edge, edge.target, node2)
+                                // if(this.includes_Edge(edge2)==false){
+                                //     // console.log("yo")
+                                //     this.add_Edge(edge2)
+                                // }
 
-                                /** à décommenter pour faire une estimation du temps qu'il faut pour completer tous le graphe */
+                                
+
+                                /** à décommenter pour voir plus d'information sur le graphe */
                                 // console.log("nodes :")
                                 // console.log(this.nodes.length)
                                 // console.log("edges :")
@@ -218,14 +227,16 @@ class Graph {
                                 
                             })
                         }
-                    }      
-                    // let portOK = await open_Port(edge.target.label)
-                    // console.log(portOK)                  
+                    }            
                 });
-                console.log("chargement " + iteration)
                 iteration--
+
+                /**optionnelle */
+                console.log("fin de la " + ctpIteration + " iteration")
+
+
+                ctpIteration++
             }
-            
         }
     }
   }
@@ -280,7 +291,7 @@ function testE(){
 
 /** test graph*/
 async function testG(){
-    let n1 = new Node(1,"5.135.143.111")
+    let n1 = new Node(0,"5.135.143.111")
     let n2 = new Node(2, "5.135.143.112")
     let n3 = new Node(3, "5.135.143.12")
 
@@ -301,15 +312,25 @@ async function testG(){
     await g.graphInit(2)
     // console.log(g)
 
-    // indispensable pour laisser le graphe se construire => pour faire une estimation du temps il faut 
+    // indispensable pour laisser le graphe se construire => pour faire une estimation du temps qu'il faut
     await timeout(30000);
+    // calcul rapide : 25440 noeud et node pour 30 sec
 
-    console.log("nodes final:")
-    console.log(g.nodes.length)
-    console.log("edges final :")
-    console.log(g.edges.length)
+    // console.log("nodes final:")
+    // console.log(g.nodes.length)
+    // console.log("edges final :")
+    // console.log(g.edges.length)
 
-    exportDataYAML(g, "graphe1")
+    // exportDataYAML(g, "graphe2")
+    exportDataJSON(g, "graphe2")
 
+    
+
+
+    // c = Object.toString(e1) + Object.toString(e2)
+    // console.log(c)
+    // let b = "maitre"
+    // let a = ["maitre", 15,5 ]
+    // console.log(a.includes(b))
 }
 testG()
