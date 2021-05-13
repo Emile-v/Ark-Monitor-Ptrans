c9b69ad3fe0f5633e9a47879e3d076d419662efe
Friviere <florent.riviere@etu.univ-nantes.fr>
2021-05-13 13:14:38 +0200

    utiliser http-server localement test

--- utils/NetGraph.js
+++ utils/NetGraph.js
@@ -3,7 +3,7 @@ const {exec} = require('child_process');
 
 async function LaunchGraph(){
 
-    const child = exec('http-server sigma/ '); 
+    const child = exec('npm start'); 
     setTimeout(function() {console.log("Please head over to the address above to visualize your network graph.")},1000)
 
     child.stdout.on('data', (data) => {
