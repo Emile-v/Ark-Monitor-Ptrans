280e5045ea587db691d6bcd05b848be03ef2816e
Friviere <florent.riviere@etu.univ-nantes.fr>
2021-05-13 10:41:29 +0200

    affichage machinespec

--- Indicators/Local/getMachineSpec.js
+++ Indicators/Local/getMachineSpec.js
@@ -14,9 +14,26 @@ const os = require('os');
     data.osplatform=os.platform();
     data.osarch=os.arch();
     data.cpus=os.cpus();
-    data.netinf=os.networkInterfaces();
-    data.freemem=os.freemem();
-    data.totalmem= os.totalmem();
+    data.cpus.forEach(element => {     delete element.times;        }); //deleting times object
+    let netinf=os.networkInterfaces();
+
+    // transforming the object to display it fully
+    for (const [key, value] of Object.entries(netinf)) {
+      console.log(`${key}: ${value}`);      
+      value.forEach(valelem => {
+        data.netinf.push({
+        name: key,
+        address: valelem.address,
+        netmask: valelem.netmask,
+        family: valelem.family,
+        mac: valelem.mac,
+        internal: valelem.internal,
+        cidr: valelem.cidr})  ;
+        
+      });
+    }
+    data.freemem=os.freemem()/1024 + " mb" ;
+    data.totalmem= os.totalmem()/1024+ " mb";
     return data;
   }
 
