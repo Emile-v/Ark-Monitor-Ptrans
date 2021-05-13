53c0479a152e1f6b9976a04827e92579677905cf
Friviere <florent.riviere@etu.univ-nantes.fr>
2021-05-13 19:50:04 +0200

    path globale pour sigma

--- utils/NetGraph.js
+++ utils/NetGraph.js
@@ -1,12 +1,13 @@
 var liveServer = require("live-server");
-
+const path = require('path')
 async function LaunchGraph(){
 
     setTimeout(function() {console.log("Please head over to the address above to visualize your network graph.")},1000)
     var params = {
       port: 8080, // Set the server port. Defaults to 8080.
       host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-      root: "./sigma", // Set root directory that's being served. Defaults to cwd.
+	 
+      root: path.join(__dirname, '../sigma'), // Set root directory that's being served. Defaults to cwd.
       open: true, // When false, it won't load your browser by default.
       ignore: 'scss,my/templates', // comma-separated string for paths to ignore
       file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
