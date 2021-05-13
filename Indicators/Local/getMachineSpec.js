7b63fc07a2f4c64ba9003e39fe04783eef7231bb
Friviere <florent.riviere@etu.univ-nantes.fr>
2021-05-13 09:59:33 +0200

    clean merge

--- Indicators/Local/getMachineSpec.js
+++
@@ -1,7 +0,0 @@
-function getMachineSpec(){
-    console.log(os.type()+ " " + os.platform() + "  " + os.arch());
-    console.log(os.cpus()); 
-    console.log(os.networkInterfaces()); 
-    console.log(os.freemem() +" / "+ os.totalmem());
-    
-  }
