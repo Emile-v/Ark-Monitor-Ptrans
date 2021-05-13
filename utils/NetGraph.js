05b4723b3f0da8e1d31971c799e6b290f546c2e7
Friviere <florent.riviere@etu.univ-nantes.fr>
2021-05-13 19:30:57 +0200

    liveserver facon node

--- utils/NetGraph.js
+++ utils/NetGraph.js
@@ -1,22 +1,25 @@
-const {exec} = require('child_process');
-
+var liveServer = require("live-server");
 
 async function LaunchGraph(){
 
-    const child = exec('npm start'); 
     setTimeout(function() {console.log("Please head over to the address above to visualize your network graph.")},1000)
+    var params = {
+      port: 8080, // Set the server port. Defaults to 8080.
+      host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+      root: "./sigma", // Set root directory that's being served. Defaults to cwd.
+      open: true, // When false, it won't load your browser by default.
+      ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+      file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+      wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+      mount: [['/components', './node_modules']], // Mount a directory to a route.
+      logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+      middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+  };
+  liveServer.start(params);
 
-    child.stdout.on('data', (data) => {
-        
-        //filters the get logs to not clutter the display
-        if(!data.includes("GET")){
-          console.log(data);
-        }
-      });
-      
-      child.stderr.on('data', (data) => {
-        console.error(`Error : ${data}`);
-      });
 }
+
+
+
 
 module.exports.launch_graph = LaunchGraph;
